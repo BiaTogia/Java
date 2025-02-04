9da0b54836cb7ef8984a08402bc8b87948ff99df
BiaTogia <abdullatoghrul@gmail.com>
2025-02-04 13:56:21 +0400

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void sumPositiveNumbers(int[] numbers){
+        for(int i : numbers){
+            if(i > 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
