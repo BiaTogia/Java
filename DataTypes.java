017cec98050065e64c30b9fb686c0e9c053ba857
BiaTogia <abdullatoghrul@gmail.com>
2025-02-04 13:02:25 +0400

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,8 @@
+public class DataTypes {
+    public static void dataType(int i, double d, char c, boolean b){
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+}
